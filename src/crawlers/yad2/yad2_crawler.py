5ef28838a8fe7e56db748ca05fd50baccc6335a4
Bar Zrihan <bzrihan@drivenets.com>
2018-12-18 23:11:20 +0200

    Use Threading in Crawler Yad2

--- src/crawlers/yad2/yad2_crawler.py
+++ src/crawlers/yad2/yad2_crawler.py
@@ -5,11 +5,49 @@ import logging
 import os
 import re
 import requests
+import threading
 
 logger = logging.getLogger(__name__)
 
 
-class Crawler:
+class FileWorker(threading.Thread):
+
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs=None, *, daemon=True):
+        super().__init__(group=group, target=target, name=name,
+                         daemon=daemon)
+        self.args = args
+        self.kwargs = kwargs
+        self.stop = False
+
+        self.properties_per_page = self.kwargs.pop("properties_per_page")
+
+    @staticmethod
+    def gen_file(feeds):
+        f_name = "feeds_1.json"
+        while os.path.exists(f_name):
+            num = re.match(r".+_([\d]+).+", f_name).group(1)
+            f_name = f_name.replace(num, str(int(num) + 1))
+
+        logger.info(f"Creating new file: {f_name}.")
+        with open(f_name, "a") as f:
+            json.dump(feeds, f, ensure_ascii=False)
+
+    def run(self):
+        logger.info("FileWorker is waiting for enough jobs to dump to file..")
+        feed = self.args[0]
+        while not self.stop:
+            while len(feed) > self.properties_per_page:
+                keys = list(feed.keys())[:self.properties_per_page]
+                dump = {k: feed.pop(k) for k in keys}
+                # when gets to the number of page that the user wants, dump current feed to file
+                self.gen_file(dump)
+        if feed:
+            # dump all feeds that left. properly less than "self.properties_per_page"
+            self.gen_file(feed)
+
+
+class PageWorker(threading.Thread):
     HEADERS = {
         "Host": "www.yad2.co.il",
         "Connection": "keep-alive",
@@ -25,49 +63,36 @@ class Crawler:
     URL_ITEM = "http://www.yad2.co.il/api/item/{id}"
     URL_ADDI_INFO = "http://www.yad2.co.il/api/item/{id}/additionalinfo"
 
-    def __init__(self, properties_per_page=1000):
-        self.properties_per_page = properties_per_page
-        self.more_pages = True
-        self.page = 1
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs=None, *, daemon=True):
+        super().__init__(group=group, target=target, name=name,
+                         daemon=daemon)
+        self.args = args
+        self.kwargs = kwargs
+        self.stop = False
+        self.reach_end = False
 
-    @staticmethod
-    def gen_file(feeds):
-        f_name = "feeds_1.json"
-        while os.path.exists(f_name):
-            num = re.match(r".+_([\d]+).+", f_name).group(1)
-            f_name = f_name.replace(num, str(int(num) + 1))
-
-        logger.info(f"Creating new file: {f_name}.")
-        with open(f_name, "a") as f:
-            json.dump(feeds, f, ensure_ascii=False)
+        self.feed = self.kwargs.pop("feed")
 
     def run(self):
+        page = self.args[0]
+        try:
+            page_info = self.get_page(page)
+        except ValueError:
+            logger.info("Stop reading")
+            self.reach_end = True
+            return
 
-        feed = {}
-        while self.more_pages:
-            page_info = self.get_page(self.page)
-            self.page += 1
+        if isinstance(page_info, int):
+            # Whoops, looks like something went wrong.
+            # Try again
+            page_info = self.get_page(page - 1)
 
-            if isinstance(page_info, int):
-                # Whoops, looks like something went wrong.
-                # Try again
-                page_info = self.get_page(self.page - 1)
-
-            for _property in page_info:
-                data = self.yad2_additions(property_id=str(_property['link_token']))
-                if not isinstance(data, int):
-                    _property.update(data)
-                    feed[_property["id"]] = _property
-
-            while len(feed) > self.properties_per_page:
-                keys = list(feed.keys())[:self.properties_per_page]
-                dump = {k: feed.pop(k) for k in keys}
-                # when gets to the number of page that the user wants, dump current feed to file
-                self.gen_file(dump)
-
-        if feed:
-            # dump all feeds that left. properly less than "self.properties_per_page"
-            self.gen_file(feed)
+        for _property in page_info:
+            data = self.yad2_additions(property_id=str(_property['link_token']))
+            if not isinstance(data, int):
+                _property.update(data)
+                self.feed[_property["id"]] = _property
 
     def get_page(self, page_num):
         res = requests.get(self.URL_FORSALE.format(page_num), headers=self.HEADERS)
@@ -82,7 +107,7 @@ class Crawler:
 
         if not len(items):
             logger.info("Stop reading pages, no more pages to read")
-            self.more_pages = False
+            raise ValueError
 
         return items
 
@@ -111,11 +136,41 @@ class Crawler:
         return {"info": items, "additional_info": items1}
 
 
+class Crawler:
+    def __init__(self, properties_per_page=1000, max_workers=8):
+        self.properties_per_page = properties_per_page
+        self.max_workers = max_workers
+        self.more_pages = True
+        self.page = 1
+
+    def run(self):
+        logger.info(f"Running Crawler.")
+        feed = {}
+
+        t = FileWorker(args=(feed,), kwargs={'properties_per_page': self.properties_per_page})
+        t.start()
+
+        while self.more_pages:
+            t_list = []
+            for number in range(self.max_workers):
+                t = PageWorker(args=(self.page,), kwargs={"feed": feed})
+                t.start()
+                t_list.append(t)
+                self.page += 1
+
+            [t.join() for t in t_list]
+            if any([t.reach_end for t in t_list]):
+                self.more_pages = False
+                t.stop = True
+                t.join()
+                break
+
+
 if __name__ == '__main__':
     c_handler = logging.StreamHandler(sys.stdout)
-    c_format = logging.Formatter('%(asctime)s [ %(funcName)-15s] %(levelname)s: %(message)s')
+    c_format = logging.Formatter('%(asctime)s [ %(funcName)-15s] %(levelname)s: (%(threadName)-10s) %(message)s')
     c_handler.setFormatter(c_format)
-    logger.setLevel(level=logging.DEBUG)
+    logger.setLevel(level=logging.INFO)
     logger.addHandler(c_handler)
 
     c = Crawler()
