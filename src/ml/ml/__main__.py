2bf8455a6705593cff0af1b2de6ad057f2d973ab
Bar Zrihan <bzrihan@drivenets.com>
2019-05-08 12:27:41 +0300

    Add more apis to gag ml server for the UI

--- src/ml/ml/__main__.py
+++ src/ml/ml/__main__.py
@@ -1,9 +1,17 @@
-from flask import Flask
+import json
 
+from flask import Flask
 from ml_api import api
 
 app = Flask(__name__)
 app.register_blueprint(api)
 
+
+@app.route("/site_map")
+def site_map():
+    rules = app.url_map.iter_rules()
+    return json.dumps(sorted([str(rule) for rule in rules]))
+
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
