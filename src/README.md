d55c1b669b6bd79067cd6000ccbda871290e59fa
Bar Zrihan <bzrihan@drivenets.com>
2019-05-08 12:27:41 +0300

    Add more apis to gag ml server for the UI

--- src/README.md
+++ src/README.md
@@ -1,2 +1,11 @@
 # Final Project
-Client apps repo
+## ML Server
+### Building
+NOTE: Need to be at the location of 'src/ml' (The location of the 'Dockerfile')
+```bash
+docker build -t gag_ml:latest .
+``` 
+### Run it
+```bash
+docker run -h gag_ml -d -p 5000:5000 gag_ml:latest
+```
--- src/ml/ml/ml_api.py
+++ src/ml/ml/ml_api.py
@@ -1,3 +1,4 @@
+import codecs
 import json
 import os
 
@@ -11,14 +12,77 @@ API_PATH = "/v1/api"
 api = Blueprint(API_PATH, __name__)
 
 
-def available_cities():
+def _available_cities():
     subfolders = [f.name for f in os.scandir('pickles') if f.is_dir()]
     return subfolders
 
 
+def _city_values(city):
+    # check for cache
+    json_f = os.path.join("pickles", city, "streets.json")
+    if not os.path.exists(json_f):
+        # TODO: cache should be FileSystemCache to be limited to time and not by existent of file
+        df = pd.read_csv("data/map_address_neighborhood.csv")
+        df = df.loc[city == df['city']]
+
+        content = json.dumps({
+            "street": df['street'].drop_duplicates().sort_values().get_values().tolist(),
+            "neighborhood": df['neighborhood'].drop_duplicates().sort_values().get_values().tolist()
+        }, ensure_ascii=False)
+        with codecs.open(json_f, 'wb', encoding='utf-8') as f:
+            f.write(content)
+
+    with open(json_f, 'r', encoding='utf-8') as f:
+        content = json.load(f)
+
+    return content
+
+
 @api.route(f'{API_PATH}/cities', methods=['GET'])
 def cities():
-    return json.dumps(available_cities(), ensure_ascii=False)
+    return json.dumps(_available_cities(), ensure_ascii=False)
+
+
+@api.route(f'{API_PATH}/neighborhoods/<city>', methods=['GET'])
+def neighborhood(city):
+    if city not in _available_cities():
+        return f"City '{city}'' is not available.", 404
+
+    content = _city_values(city)
+    return json.dumps(content['neighborhood'], ensure_ascii=False)
+
+
+@api.route(f'{API_PATH}/streets/<city>', methods=['GET'])
+def streets(city):
+    if city not in _available_cities():
+        return f"City '{city}'' is not available.", 404
+
+    content = _city_values(city)
+    return json.dumps(content['street'], ensure_ascii=False)
+
+
+@api.route(f'{API_PATH}/property_types', methods=['GET'])
+def property_types():
+    # TODO: should be dynamically from data
+    # df['property_type'].unique().tolist()
+    types = ['דירה בבית קומות', 'דירת גג', 'דירת מגורים', 'בית בודד',
+             "קוטג' חד משפחתי", "קוטג' דו משפחתי", 'דירת נופש', 'דירת גן']
+    return json.dumps(types, ensure_ascii=False)
+
+
+@api.route(f'{API_PATH}/build_years', methods=['GET'])
+def build_years():
+    # TODO: should be dynamically from data
+    # import time
+    # vals = df['build_year'].unique().astype(int).tolist()
+    #
+    # years = []
+    # for v in vals:
+    #     z = time.strftime("%Y", time.gmtime(v/1000000000))
+    #     years.append(z)
+    # years = sorted(years)
+    # print(f"From year: {years[0]}, to year: {years[-1]}")
+    return json.dumps(range(1800, 2023))  # statically from 1800 - 2022
 
 
 @api.route(f'{API_PATH}/predict', methods=['POST'])
@@ -29,9 +93,8 @@ def predict():
     data = content['data']
     algo_name = content.get('algo', 'algo')
 
-    if city not in available_cities():
-        # TODO: return error code
-        raise ValueError("City is not available.")
+    if city not in _available_cities():
+        return f"City '{city}'' is not available.", 404
 
     processor = joblib.load(os.path.join("pickles", city, "processor.joblib"))
     algo = joblib.load(os.path.join("pickles", city, f"{algo_name}.joblib"))
